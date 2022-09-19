a047e5feb7bf397f4df5bc62b96a37a07bd49880
huni0420 <mysky0420@naver.com>
2022-09-20 06:21:17 +0900

    feat: Main페이지에 Dungeon가기 링크추가, ChaosFloor에 canvas element추가 canvas element에 context메서드에 2d추가 canvas element에 keyDown이벤트를 추가하고, 이벤트를 이용해서 움직이는 구체 표현

--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Main() {
-    return <>
-    main
-    </>
+    return (
+        <>
+        <section>
+            <div>
+                <Link to={'/DungeonFloor'}>던전가기</Link>
+            </div>   
+            <div>
+                <Link>명예의 전당</Link>
+            </div>   
+        </section>
+        </>
+    )
 }
